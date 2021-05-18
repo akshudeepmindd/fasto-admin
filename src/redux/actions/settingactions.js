1d062b0d7cba1b0ba801f5c633467e83a8d9416a
mastro <25ayushi1995@gmail.com>
2021-05-18 14:05:36 +0530

    change in settings

--- src/redux/actions/settingactions.js
+++ src/redux/actions/settingactions.js
@@ -1,37 +1,37 @@
-import * as settingApis from "../../services/setting";
-import * as settingActionTypes from "../actionsType/settingActionType";
+import * as settingApis from '../../services/setting'
+import * as settingActionTypes from '../actionsType/settingActionType'
 
 export const createSetting = (params) => async (dispatch) => {
-  const resp = await settingApis.newSetting(params);
+  const resp = await settingApis.newSetting(params)
   dispatch({
     type: settingActionTypes.CREATE_SETTING,
     payload: resp.data,
-  });
-  return resp.data;
-};
+  })
+  return resp.data
+}
 
 export const updateSetting = (params) => async (dispatch) => {
-  const resp = await settingApis.updateSetting(params);
+  const resp = await settingApis.updateSetting(params)
   dispatch({
     type: settingActionTypes.UPDATE_SETTING,
     payload: resp.data,
-  });
-  return resp.data;
-};
+  })
+  return resp.data
+}
 
 export const deleteSetting = (params) => async (dispatch) => {
-  const resp = await settingApis.deleteSetting(params);
+  const resp = await settingApis.deleteSetting(params)
   dispatch({
     type: settingActionTypes.DELETE_SETTING,
     payload: resp.data,
-  });
-  return resp.data;
-};
+  })
+  return resp.data
+}
 
 export const getAllSettings = () => async (dispatch) => {
-  const resp = await settiApis.getSettings();
+  const resp = await settingApis.getSettings()
   dispatch({
     type: settingActionTypes.ALL_SETTINGS,
     payload: resp?.data?.data,
-  });
-};
+  })
+}
